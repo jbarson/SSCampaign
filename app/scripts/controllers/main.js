04a56580d616fe71a745d80b93f5f0375fa36036
Jon Barson <jbarson@gmail.com>
2013-08-31 18:15:04 -0400

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('SSCampaignApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
